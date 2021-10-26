9425446e3b19bba2a0e7cd3161d8ad2f1e9339ce
Vantrono <382985@colum.edu>
2021-10-26 17:13:33 -0500

    InnerDungeon Texture+Lights

--- QuakeDev/working/compile.bat
+++ QuakeDev/working/compile.bat
@@ -17,6 +17,9 @@ C:\Users\Dat\Qwoke\QuakeDev\tools\ericw-tools\bin\qbsp.exe SplitQwake
 echo --------------VIS---------------
 C:\Users\Dat\Qwoke\QuakeDev\tools\ericw-tools\bin\vis.exe SplitQwake
 
+echo -------------LIGHT--------------
+C:\Users\Dat\Qwoke\QuakeDev\tools\ericw-tools\bin\light.exe -soft -extra4 SplitQwake
+
 copy SplitQwake.bsp C:\Users\Dat\Qwoke\QuakeDev\id1\maps
 copy SplitQwake.pts C:\Users\Dat\Qwoke\QuakeDev\id1\maps
 copy SplitQwake.lit C:\Users\Dat\Qwoke\QuakeDev\id1\maps

6dd67514dcc5216e56eea7cb744fb6a258a4db53
Vantrono <382985@colum.edu>
2021-11-02 11:38:22 -0500

    Lights and Weapon placements

--- QuakeDev/working/compile.bat
+++ QuakeDev/working/compile.bat
@@ -1,25 +1,25 @@
 @echo off
 
 cd\
-cd C:\Users\382985\Documents\GitHub\Qwoke\QuakeDev\working
+cd C:\Users\Dat\Qwoke\QuakeDev\working
 
 
 echo Copying Files...
-copy C:\Users\382985\Documents\GitHub\Qwoke\QuakeDev\id1\maps\SplitQwake.map C:\Users\382985\Documents\GitHub\Qwoke\QuakeDev\working
+copy C:\Users\Dat\Qwoke\QuakeDev\id1\maps\SplitQwake.map C:\Users\Dat\Qwoke\QuakeDev\working
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-C:\Users\382985\Documents\GitHub\Qwoke\QuakeDev\tools\ericw-tools\bin\qbsp.exe SplitQwake
+C:\Users\Dat\Qwoke\QuakeDev\tools\ericw-tools\bin\qbsp.exe SplitQwake
 
 echo --------------VIS---------------
-C:\Users\382985\Documents\GitHub\Qwoke\QuakeDev\tools\ericw-tools\bin\vis.exe SplitQwake
+C:\Users\Dat\Qwoke\QuakeDev\tools\ericw-tools\bin\vis.exe SplitQwake
 
 echo -------------LIGHT--------------
-C:\Users\382985\Documents\GitHub\Qwoke\QuakeDev\tools\ericw-tools\bin\light.exe -soft -extra4 SplitQwake
+C:\Users\Dat\Qwoke\QuakeDev\tools\ericw-tools\bin\light.exe -soft -extra4 SplitQwake
 
-copy SplitQwake.bsp C:\Users\382985\Documents\GitHub\Qwoke\QuakeDev\id1\maps
-copy SplitQwake.pts C:\Users\382985\Documents\GitHub\Qwoke\QuakeDev\id1\maps
-copy SplitQwake.lit C:\Users\382985\Documents\GitHub\Qwoke\QuakeDev\id1\maps
+copy SplitQwake.bsp C:\Users\Dat\Qwoke\QuakeDev\id1\maps
+copy SplitQwake.pts C:\Users\Dat\Qwoke\QuakeDev\id1\maps
+copy SplitQwake.lit C:\Users\Dat\Qwoke\QuakeDev\id1\maps
--- QuakeDev/working/run.bat
+++ QuakeDev/working/run.bat
@@ -1,3 +1,3 @@
 cd\
-cd C:\Users\382985\Documents\GitHub\Qwoke\QuakeDev
+cd C:\Users\Dat\Qwoke\QuakeDev
 quakespasm-spiked-win64  +map SplitQwake
